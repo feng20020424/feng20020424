9ecee0998170022df54faf5c407a35fdb2a2639e
jian-feng01 <2547138412@qq.com>
2022-08-19 01:16:48 +0800

    日常练习

---
+++ pailie/pailie/源.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int	b = 0;
+	int	c = 0;
+	scanf("%d %d %d",&a, &b, &c);
+	if (a < b)
+	{
+		int d = a;
+		a = b;
+		b = d;
+	}
+	if (a < c)
+	{
+		int d = a;
+		a = c;
+		c = d;
+	}
+	if (b < c)
+	{
+		int d = b;
+		b = c;
+		c = d;
+	}
+	printf("%d %d %d\n", a, b, c);
+	return 0;
+
+}
