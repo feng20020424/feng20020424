3d730e8f0cd0d3bd7e1727692f660b1c901557ed
jian-feng01 <2547138412@qq.com>
2022-10-21 23:25:29 +0800

    练习

---
+++ build-test8-Desktop_Qt_5_14_0_MinGW_32_bit-Debug/ui_widget.h
@@ -0,0 +1,61 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QTreeWidget>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QVBoxLayout *verticalLayout;
+    QTreeWidget *treeWidget;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName(QString::fromUtf8("Widget"));
+        Widget->resize(800, 600);
+        verticalLayout = new QVBoxLayout(Widget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        treeWidget = new QTreeWidget(Widget);
+        QTreeWidgetItem *__qtreewidgetitem = new QTreeWidgetItem();
+        __qtreewidgetitem->setText(0, QString::fromUtf8("1"));
+        treeWidget->setHeaderItem(__qtreewidgetitem);
+        treeWidget->setObjectName(QString::fromUtf8("treeWidget"));
+
+        verticalLayout->addWidget(treeWidget);
+
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QCoreApplication::translate("Widget", "Widget", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
