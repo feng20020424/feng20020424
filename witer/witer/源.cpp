ee78cc04ee7680280af1b93ba81bc9abab371f9f
jian-feng01 <2547138412@qq.com>
2022-10-24 23:31:28 +0800

    练习

--- witer/witer/源.cpp
+++ witer/witer/源.cpp
@@ -1,15 +1,75 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+#include<string.h>
+#define N 100
+int main() 
+{
 
-int main() {
-    float h, r;
-    scanf("%f%f", &h, &r);
-    float l = 3.14 * h * r * r;
-    int n = 0;
-    for (n = 0; l * n < 10000; n++)
+    char arr[N] = {0};
+    scanf("%s",arr);
+    int n = strlen(arr);
+    for (int i = 0; i < n; i++)
     {
-
+        if (arr[i]% 2 == 0)
+        {
+            arr[i] = '0';
+        }
+        else
+        {
+            arr[i] = '1';
+        }
     }
-    printf("%d", n);
+
+    long long num = 0;
+
+    for (int i = 0; i < strlen(arr); i++)
+    {
+        num = num * 10 + (arr[i] - '0');
+    }
+    printf("%lld\n", num);
+
+
+    
+    
     return 0;
 }
+//int main(void)
+//{
+//    int i;
+//    char temp[N];
+//    int count = 0;
+//    long int number = 0L;
+//
+//    scanf("%10s", temp);
+//    for (i = 0; i < strlen(temp); i++)
+//    {
+//        if (0 == (temp[i] - '0') % 2)
+//        {
+//            temp[i] = '0';
+//        }
+//        else
+//        {
+//            temp[i] = '1';
+//        }
+//    }
+//    for (i = 0; i < strlen(temp); i++)
+//    {
+//        number = number * 10 + (temp[i] - '0');
+//    }
+//    printf("%ld\n", number);
+//
+//    return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int num = 0;
+//	while (a)
+//	{
+//		num = num * 10 + (a % 10) % 2;
+//		a = a / 10;
+//	}
+//	printf("%d", num);
+//	return 0;
+//}
