04c5dd2cd58756c0aa3d4f302110f70dc2c8cbaf
jian-feng01 <2547138412@qq.com>
2022-10-27 23:19:50 +0800

    练习

---
+++ build-test9-Desktop_Qt_5_14_0_MinGW_32_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,261 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QRadioButton>
+#include <QtWidgets/QScrollArea>
+#include <QtWidgets/QStackedWidget>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
+#include <QtWidgets/QToolBox>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QStackedWidget *stackedWidget;
+    QWidget *page_4;
+    QScrollArea *scrollArea;
+    QWidget *scrollAreaWidgetContents;
+    QVBoxLayout *verticalLayout;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_10;
+    QPushButton *pushButton_11;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_9;
+    QPushButton *pushButton_8;
+    QPushButton *pushButton_5;
+    QPushButton *pushButton_6;
+    QPushButton *pushButton_7;
+    QWidget *page_5;
+    QToolBox *toolBox;
+    QWidget *page;
+    QPushButton *pushButton_12;
+    QWidget *page_2;
+    QRadioButton *radioButton;
+    QWidget *page_6;
+    QWidget *page_3;
+    QWidget *page_7;
+    QTabWidget *tabWidget;
+    QWidget *tab;
+    QWidget *tab_2;
+    QWidget *tab_3;
+    QPushButton *btn_1;
+    QPushButton *btn_2;
+    QPushButton *btn_3;
+    QComboBox *comboBox;
+    QLabel *label;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        stackedWidget = new QStackedWidget(centralwidget);
+        stackedWidget->setObjectName(QString::fromUtf8("stackedWidget"));
+        stackedWidget->setGeometry(QRect(170, 50, 241, 341));
+        page_4 = new QWidget();
+        page_4->setObjectName(QString::fromUtf8("page_4"));
+        scrollArea = new QScrollArea(page_4);
+        scrollArea->setObjectName(QString::fromUtf8("scrollArea"));
+        scrollArea->setGeometry(QRect(40, 20, 113, 301));
+        scrollArea->setWidgetResizable(true);
+        scrollAreaWidgetContents = new QWidget();
+        scrollAreaWidgetContents->setObjectName(QString::fromUtf8("scrollAreaWidgetContents"));
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 111, 386));
+        verticalLayout = new QVBoxLayout(scrollAreaWidgetContents);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        pushButton = new QPushButton(scrollAreaWidgetContents);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+
+        verticalLayout->addWidget(pushButton);
+
+        pushButton_2 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+
+        verticalLayout->addWidget(pushButton_2);
+
+        pushButton_10 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_10->setObjectName(QString::fromUtf8("pushButton_10"));
+
+        verticalLayout->addWidget(pushButton_10);
+
+        pushButton_11 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_11->setObjectName(QString::fromUtf8("pushButton_11"));
+
+        verticalLayout->addWidget(pushButton_11);
+
+        pushButton_3 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+
+        verticalLayout->addWidget(pushButton_3);
+
+        pushButton_4 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+
+        verticalLayout->addWidget(pushButton_4);
+
+        pushButton_9 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_9->setObjectName(QString::fromUtf8("pushButton_9"));
+
+        verticalLayout->addWidget(pushButton_9);
+
+        pushButton_8 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_8->setObjectName(QString::fromUtf8("pushButton_8"));
+
+        verticalLayout->addWidget(pushButton_8);
+
+        pushButton_5 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_5->setObjectName(QString::fromUtf8("pushButton_5"));
+
+        verticalLayout->addWidget(pushButton_5);
+
+        pushButton_6 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_6->setObjectName(QString::fromUtf8("pushButton_6"));
+
+        verticalLayout->addWidget(pushButton_6);
+
+        pushButton_7 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_7->setObjectName(QString::fromUtf8("pushButton_7"));
+
+        verticalLayout->addWidget(pushButton_7);
+
+        scrollArea->setWidget(scrollAreaWidgetContents);
+        stackedWidget->addWidget(page_4);
+        page_5 = new QWidget();
+        page_5->setObjectName(QString::fromUtf8("page_5"));
+        toolBox = new QToolBox(page_5);
+        toolBox->setObjectName(QString::fromUtf8("toolBox"));
+        toolBox->setGeometry(QRect(30, 20, 121, 291));
+        page = new QWidget();
+        page->setObjectName(QString::fromUtf8("page"));
+        page->setGeometry(QRect(0, 0, 121, 175));
+        pushButton_12 = new QPushButton(page);
+        pushButton_12->setObjectName(QString::fromUtf8("pushButton_12"));
+        pushButton_12->setGeometry(QRect(0, 40, 93, 28));
+        toolBox->addItem(page, QString::fromUtf8("Page 1"));
+        page_2 = new QWidget();
+        page_2->setObjectName(QString::fromUtf8("page_2"));
+        page_2->setGeometry(QRect(0, 0, 121, 175));
+        radioButton = new QRadioButton(page_2);
+        radioButton->setObjectName(QString::fromUtf8("radioButton"));
+        radioButton->setGeometry(QRect(20, 40, 110, 19));
+        toolBox->addItem(page_2, QString::fromUtf8("Page 2"));
+        page_6 = new QWidget();
+        page_6->setObjectName(QString::fromUtf8("page_6"));
+        toolBox->addItem(page_6, QString::fromUtf8("\351\241\265"));
+        page_3 = new QWidget();
+        page_3->setObjectName(QString::fromUtf8("page_3"));
+        toolBox->addItem(page_3, QString::fromUtf8("\351\241\265"));
+        stackedWidget->addWidget(page_5);
+        page_7 = new QWidget();
+        page_7->setObjectName(QString::fromUtf8("page_7"));
+        tabWidget = new QTabWidget(page_7);
+        tabWidget->setObjectName(QString::fromUtf8("tabWidget"));
+        tabWidget->setGeometry(QRect(20, 30, 211, 261));
+        tab = new QWidget();
+        tab->setObjectName(QString::fromUtf8("tab"));
+        tabWidget->addTab(tab, QString());
+        tab_2 = new QWidget();
+        tab_2->setObjectName(QString::fromUtf8("tab_2"));
+        tabWidget->addTab(tab_2, QString());
+        tab_3 = new QWidget();
+        tab_3->setObjectName(QString::fromUtf8("tab_3"));
+        tabWidget->addTab(tab_3, QString());
+        stackedWidget->addWidget(page_7);
+        btn_1 = new QPushButton(centralwidget);
+        btn_1->setObjectName(QString::fromUtf8("btn_1"));
+        btn_1->setGeometry(QRect(460, 110, 93, 28));
+        btn_2 = new QPushButton(centralwidget);
+        btn_2->setObjectName(QString::fromUtf8("btn_2"));
+        btn_2->setGeometry(QRect(460, 170, 93, 28));
+        btn_3 = new QPushButton(centralwidget);
+        btn_3->setObjectName(QString::fromUtf8("btn_3"));
+        btn_3->setGeometry(QRect(460, 230, 93, 28));
+        comboBox = new QComboBox(centralwidget);
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+        comboBox->setGeometry(QRect(550, 360, 87, 22));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(200, 390, 381, 141));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 26));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        stackedWidget->setCurrentIndex(2);
+        toolBox->setCurrentIndex(2);
+        tabWidget->setCurrentIndex(2);
+
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_10->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_11->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_9->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_8->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_5->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_6->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_7->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_12->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        toolBox->setItemText(toolBox->indexOf(page), QCoreApplication::translate("MainWindow", "Page 1", nullptr));
+        radioButton->setText(QCoreApplication::translate("MainWindow", "RadioButton", nullptr));
+        toolBox->setItemText(toolBox->indexOf(page_2), QCoreApplication::translate("MainWindow", "Page 2", nullptr));
+        toolBox->setItemText(toolBox->indexOf(page_6), QCoreApplication::translate("MainWindow", "\351\241\265", nullptr));
+        toolBox->setItemText(toolBox->indexOf(page_3), QCoreApplication::translate("MainWindow", "\351\241\265", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab), QCoreApplication::translate("MainWindow", "\347\211\233\351\200\274", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab_2), QCoreApplication::translate("MainWindow", "\350\276\276\347\223\246", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab_3), QCoreApplication::translate("MainWindow", "\351\241\265", nullptr));
+        btn_1->setText(QCoreApplication::translate("MainWindow", "1", nullptr));
+        btn_2->setText(QCoreApplication::translate("MainWindow", "2", nullptr));
+        btn_3->setText(QCoreApplication::translate("MainWindow", "3", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "TextLabel", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
