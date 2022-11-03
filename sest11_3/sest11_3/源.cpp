45de384e88d4dd1a5f9582f3c44fdd4cc4ca1310
jian-feng01 <2547138412@qq.com>
2022-11-03 23:44:02 +0800

    练习

---
+++ sest11_3/sest11_3/源.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    long num = 0;
+//    long long arr[10] = { 9,99,999,9999,99999,999999,9999999,99999999,999999999,9999999999 };
+//    for (int i = 0; i < 10; i++)
+//    {
+//        num = num + arr[i];
+//    }
+//    cout << num << endl;
+//}
+//using namespace std;
+//
+//int main()
+//{
+//    int a = 0;
+//    for (a = 100; a < 1000; a++)
+//    {
+//        if (((a % 10) * (a % 10) * (a % 10)) + ((a / 10 % 10) * (a / 10 % 10) * (a / 10 % 10)) + ((a / 100) * (a / 100) * (a / 100)) == a)
+//        {
+//            cout << a << endl;
+//        }
+//    }
+//}
